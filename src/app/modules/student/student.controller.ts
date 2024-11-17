dd4a5946f510e933365a95551d500ee033a9ab1b
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-17 20:32:13 +0600

    student controller and service created

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, RequestHandler } from 'express';
+import { StudentServices } from './student.service';
+
+const createStudent: RequestHandler = async (req, res) => {
+  try {
+    const student = req.body;
+    const result = await StudentServices.createStudentIntoDB(student);
+
+    // Send a response without returning it
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Student was not created!',
+    });
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+};
