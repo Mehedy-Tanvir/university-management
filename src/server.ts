7cd6a8c2ebb3a2ec856552397df670ec1274c359
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-17 19:10:24 +0600

    ts node dev set up

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import config from './app/config';
 // getting-started.js
 import mongoose from 'mongoose';
 
-main().catch((err) => console.log(err));
-
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
@@ -13,11 +11,11 @@ async function main() {
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-main();
+main().catch((err) => console.log(err));
