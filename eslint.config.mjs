04f880d364293d8995d984872a7fa7457833831e
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-17 18:43:08 +0600

    prettier added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: { globals: globals.browser },
     rules: {
-      eqeqeq: "off",
-      "no-unused-vars": "error",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-      "@typescript-eslint/no-unused-expressions": [
-        "error",
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
         {
           allowShortCircuit: true,
           allowTernary: true,
@@ -20,10 +24,14 @@ export default [
         },
       ],
     },
+    globals: {
+      process: 'readonly',
+    },
   },
   {
-    ignores: ["node_modules/*", "dist/*"],
+    ignores: ['node_modules/*', 'dist/*'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 ];
