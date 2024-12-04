9292d3fdebd71ec72186ba2f866dfa4f09830bb6
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-12-04 22:15:15 +0600

    transaction roll back used

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
@@ -6,6 +7,7 @@ import { Student } from '../student/student.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import AppError from '../../errors/AppError';
 
 const createStudentIntoDB = async (
   password: string | undefined,
@@ -24,20 +26,40 @@ const createStudentIntoDB = async (
   const admissionSemester = await AcademicSemester.findById(
     payload.admissionSemester,
   );
-  //   manually generated id
-  userData.id = await generateStudentId(admissionSemester as TAcademicSemester);
 
-  //   create a user
-  const newUser = await User.create(userData);
+  // transaction roll back
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    //   manually generated id
+    userData.id = await generateStudentId(
+      admissionSemester as TAcademicSemester,
+    );
 
-  //   create a student
-  if (Object.keys(newUser).length) {
+    //   create a user transaction-1
+    const newUser = await User.create([userData], { session });
+
+    // newUser array
+
+    //   create a student
+    if (!newUser.length) {
+      throw new AppError(400, 'Failed to create user');
+    }
     // set id, _id as user
-    payload.id = newUser.id;
-    payload.user = newUser._id;
-
-    const newStudent = await Student.create(payload);
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id;
+    // create student transaction 2
+    const newStudent = await Student.create([payload], { session });
+    if (!newStudent.length) {
+      throw new AppError(400, 'Failed to create student');
+    }
+    await session.commitTransaction();
+    await session.endSession();
     return newStudent;
+  } catch (error) {
+    console.log(error);
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
 
