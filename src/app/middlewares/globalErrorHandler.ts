9c05b8112dc79feffcf04e0e64e68eb908c95a4e
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-12-09 19:45:11 +0600

    error handle and searchterm functionality added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import handleZodError from '../errors/handleZodError';
+import { TErrorSources } from '../interface/error';
+import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
+import AppError from '../errors/AppError';
 
 const globalErrorHandler = (
   err: any,
@@ -8,12 +16,60 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ): Response => {
-  const statusCode = err.status || 500;
-  const message = err.message || 'Something went wrong!';
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import config from './app/config';
 
 // getting-started.js
 import mongoose from 'mongoose';
+import { Server } from 'http';
+let server: Server;
 
 async function main() {
   try {
@@ -10,7 +12,7 @@ async function main() {
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
@@ -19,3 +21,17 @@ async function main() {
 }
 
 main().catch((err) => console.log(err));
+
+process.on('unhandledRejection', ()=>{
+  console.log("Unhandled rejection detected! Shutting down the server!");
+  if(server){
+    server.close((=>{
+      process.exit(1)
+    }))
+  }
+});
+
+process.on("uncaughtException", ()=>{
+  console.log("Uncaught exception detected! Shutting down!");
+  process.exit(1);
+})
